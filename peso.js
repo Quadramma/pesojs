bd4482766526ed2588b610efa649bcc2046f6774
javi <javi@quadramma.com>
2015-03-01 01:30:14 +0100

    Minified version added.

--- peso.js
+++ peso.js
@@ -173,16 +173,4 @@ $.create = function(selector) {
 	local$.value = [dom];
 	return local$;
 };
-$.str = function(obj) {
-	console.log(JSON.stringify(obj));
-};
-$.screen = {
-	bounds: function() {
-		var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-		var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-		return {
-			w: w,
-			h: h
-		};
-	}
-}
+
